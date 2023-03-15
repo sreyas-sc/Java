3676b97f92848a33fe3854879fce3ccc74d95797
Sreyas <115882857+sreyas-sc@users.noreply.github.com>
2023-03-15 14:38:28 +0530

    Add files via upload

---
+++ fib.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+class fib
+{
+    public static void main(String[] args) 
+    {
+        int a,b,c;
+        Scanner cv=new Scanner(System.in);
+        System.out.println("enter the range");
+        int n=cv.nextInt();
+        a=0;
+        b=1;
+        System.out.println(a);
+        System.out.println(b);
+        // System.out.println(c);
+        for(int i=2;i<n;i++)
+        {
+            c=a+b;
+            System.out.println(c);
+            a=b;
+            b=c;
+        }    
+    }
+}
