90af966eebd9415f66ea5d54a6d83a839ad49983
Sreyas <115882857+sreyas-sc@users.noreply.github.com>
2023-03-15 15:16:12 +0530

    Add files via upload

---
+++ palindrome.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+class palindrome {
+  public static void main(String[] args) {
+    String str;
+    String reverseStr = "";
+    Scanner s = new Scanner(System.in);
+    System.out.print("Enter a value:");
+    str = s.next();
+    int strLength = str.length();
+
+    for (int i = (strLength - 1); i >=0; --i) {
+      reverseStr = reverseStr + str.charAt(i);
+    }
+
+    if (str.toLowerCase().equals(reverseStr.toLowerCase())) {
+      System.out.println(str + " is a Palindrome String.");
+    }
+    else {
+      System.out.println(str + " is not a Palindrome String.");
+    }
+  }
+}
