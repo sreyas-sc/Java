5877d3b9dca88686a6141098bb380c5b53c2c9ac
Sreyas <115882857+sreyas-sc@users.noreply.github.com>
2023-06-21 15:18:48 +0530

    Program.java

--- Test.java
+++ Test.java
@@ -1,45 +1,50 @@
 import java.util.Scanner;
 
-public class BubbleSortStringArray {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        
-        // Read the size of the array
-        System.out.print("Enter the size of the array: ");
-        int size = scanner.nextInt();
-        
-        // Read the string array elements
-        System.out.println("Enter the string array elements:");
-        String[] array = new String[size];
-        for (int i = 0; i < size; i++) {
-            array[i] = scanner.next();
-        }
-        
-        // Sort the array using bubble sort
-        bubbleSort(array);
-        
-        // Print the sorted array
-        System.out.println("Sorted array:");
-        for (String element : array) {
-            System.out.print(element + " ");
+class CPU {
+    double price;
+
+    CPU(double price) {
+        this.price = price;
+    }
+
+    static class Processor {
+        int numberOfCores;
+        String manufacturer;
+
+        Processor(int n, String m) {
+            numberOfCores = n;
+            manufacturer = m;
         }
     }
-    
-    public static void bubbleSort(String[] array) {
-        int n = array.length;
-        
-        // Perform n-1 iterations
-        for (int i = 0; i < n - 1; i++) {
-            // Perform one iteration of bubble sort
-            for (int j = 0; j < n - i - 1; j++) {
-                // Compare adjacent elements and swap if necessary
-                if (array[j].compareTo(array[j + 1]) > 0) {
-                    String temp = array[j];
-                    array[j] = array[j + 1];
-                    array[j + 1] = temp;
-                }
-            }
+
+    static class RAM {
+        int memory;
+        String manufacturer;
+
+        RAM(int memory, String manufacturer) {
+            this.memory = memory;
+            this.manufacturer = manufacturer;
         }
     }
 }
 
+public class Programs {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the CPU price: ");
+        CPU cpu = new CPU(input.nextDouble());
+
+        System.out.println("Enter the number of cores and CPU manufacturer: ");
+        CPU.Processor processor = new CPU.Processor(input.nextInt(), input.next());
+
+        System.out.println("Enter the RAM size and manufacturer: ");
+        CPU.RAM ram = new CPU.RAM(input.nextInt(), input.next());
+
+        System.out.println("CPU DETAILS: ");
+        System.out.println("Price: " + cpu.price);
+        System.out.println("CPU Cores: " + processor.numberOfCores);
+        System.out.println("CPU Manufacturer: " + processor.manufacturer);
+        System.out.println("RAM Size: " + ram.memory);
+        System.out.println("RAM Manufacturer: " + ram.manufacturer);
+    }
+}
