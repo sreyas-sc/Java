997690fdadd7396e693da6b7424d59f43b679c39
Sreyas <115882857+sreyas-sc@users.noreply.github.com>
2023-10-09 14:21:24 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,15 +1,22 @@
 import matplotlib.pyplot as plt
 
-# Programming languages and their popularity
-languages = ['Java', 'Python', 'PHP', 'JavaScript', 'C#', 'C++']
-popularity = [22.2, 17.6, 8.8, 8, 7.7, 6.7]
+# Sample Data
+math_marks = [88, 92, 80, 89, 100, 80, 60, 100, 80, 34]
+science_marks = [35, 79, 79, 48, 100, 88, 32, 45, 20, 30]
+marks_range = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
 
-# Create a pie chart
-plt.figure(figsize=(8, 8))
-plt.pie(popularity, labels=languages, autopct='%1.1f%%', startangle=140)
+# Create a scatter plot
+plt.scatter(math_marks, science_marks, c=marks_range, cmap='viridis', marker='o')
 
-# Add a title
-plt.title('Popularity of Programming Languages')
+# Add labels and title
+plt.xlabel('Math Marks')
+plt.ylabel('Science Marks')
+plt.title('Scatter Plot of Math vs. Science Marks')
 
-# Display the pie chart
+# Add a colorbar to indicate the marks range
+cbar = plt.colorbar()
+cbar.set_label('Marks Range')
+
+# Show the plot
 plt.show()
+
