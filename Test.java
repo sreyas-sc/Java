c6dcb754ac47d38181e01a6a80729fc04a69ce9e
Sreyas <115882857+sreyas-sc@users.noreply.github.com>
2023-10-30 14:23:01 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,23 +1,10 @@
-pip install matplotlib pandas
-
-import matplotlib.pyplot as plt
 import pandas as pd
 
-# Read the data from the CSV file
-data = pd.read_csv('medal.csv')
-
-# Sort the data by gold medals in descending order
-data = data.sort_values(by='gold_medal', ascending=False)
-
-# Select the top 5 countries
-top_countries = data.head(5)
-
-# Create a pie chart
-plt.figure(figsize=(8, 8))
-plt.pie(top_countries['gold_medal'], labels=top_countries['country'], autopct='%1.1f%%', startangle=140)
-
-# Add a title
-plt.title('Gold Medal Achievements (Top 5 Countries) - 2016 Summer Olympics')
-
-# Display the pie chart
-plt.show()
+data = {
+    'Name': ['Alice', 'Bob', 'Charlie', 'David', 'Eva'],
+    'Age': [30, 25, 22, 30, 28],
+    'City': ['New York', 'Los Angeles', 'Chicago', 'New York', 'Los Angeles']
+}
+df = pd.DataFrame(data)
+sorted_df = df.sort_values(by=['Age', 'City'], ascending=[True, True])
+print(sorted_df)
