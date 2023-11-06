aee925998b37da9670738e610e4ef8cb596e7034
Sreyas <115882857+sreyas-sc@users.noreply.github.com>
2023-11-06 09:45:00 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,15 +1,33 @@
 
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
 
-import pandas as pd
+# Sample data (replace this with your dataset)
+X = np.array([[2.7810836, 2.550537003],
+              [1.465489372, 2.362125076],
+              [3.396561688, 4.400293529],
+              [1.38807019, 1.850220317],
+              [3.06407232, 3.005305973],
+              [7.627531214, 2.759262235],
+              [5.332441248, 2.088626775],
+              [6.922596716, 1.77106367]])
 
-data = {
-    'Name': ['Alice', 'Bob', 'Charlie'],
-    'Age': [25, 30, 22]
-}
+y = np.array([0, 0, 0, 0, 0, 1, 1, 1])
 
-custom_index = ['one', 'two', 'three']
-df = pd.DataFrame(data, index=custom_index)
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-df_reset = df.reset_index(drop=True)
+# Create and train the KNN model
+k = 3
+knn = KNeighborsClassifier(n_neighbors=k)
+knn.fit(X_train, y_train)
 
-print(df_reset)
+# Make predictions
+y_pred = knn.predict(X_test)
+
+# Evaluate the model
+accuracy = accuracy_score(y_test, y_pred)
+print(f'Accuracy: {accuracy * 100:.2f}%')
+```
